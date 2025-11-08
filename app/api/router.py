cb334542117c78f03ca60133548ac123fc85ae05
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-11-09 00:54:49 +0530

    refactor: moved  the business logic into a dedicated service

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, HTTPException
-import requests
-from typing import List
-from bs4 import BeautifulSoup
+from fastapi import APIRouter
+from app.tasks.newsletter_tasks import start_substack_analysis
+
 
 router = APIRouter(tags=["newsletter"])
 
@@ -26,38 +25,13 @@ search_results = [
     }
 ]
 
-MAX_ISSUES = 5
-
 @router.get("/")
 def health_check():
     return { "status": "ok", "service": "scrapper" }
 
-@router.get("/scrape")
-def scrape():
-    links = []
+@router.get("/trigger-analysis")
+def trigger_analysis():
     for result in search_results:
-        if "substack" in result["link"]:
-            archive_link = f"{result["link"]}archive?sort=new"
-            r = requests.get(archive_link, timeout=10)
-            if r.status_code != 200:
-                return []
-            html = r.text
+        start_substack_analysis.delay(result)
 
-            soup = BeautifulSoup(html, "html.parser")
-            newsletter_title_container = soup.find("div", attrs={"data-testid": "navbar"})
-            newsletter_title = newsletter_title_container.find("h1").find("a").text.strip()
-            if newsletter_title == "":
-                newsletter_title = newsletter_title_container.find("h1").find("img")["alt"].strip()
-
-            issue_list_container = soup.find("div", class_="portable-archive-list")
-            issue_links = [
-                a["href"] for a in issue_list_container.find_all("a", attrs={"data-testid": 'post-preview-title'})[:MAX_ISSUES]
-                if result["link"] in a["href"]
-            ]
-            newsletter_info = {
-                "title": newsletter_title,
-                "links": issue_links
-            }
-            links.append(newsletter_info)
-
-    return { "newsletter_info": links }
+    return { "status": "Scraping triggered" }
---
+++ app/services/substack.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+from fastapi import HTTPException
+import requests
+from typing import Dict, List, Tuple
+
+
+MAX_ISSUES = 5
+
+def get_substack_newsletter_archive(search_result: Dict) -> Dict:
+    if "substack" not in search_result["link"]:
+        return {}
+
+    archive_link = f"{search_result["link"]}/api/v1/archive?sort=new&search=&offset=0&limit={MAX_ISSUES}"
+    r = requests.get(archive_link, timeout=10)
+    if r.status_code != 200:
+        return {}
+
+    issues = r.json()
+    newsletter = {}
+    newsletter["title"] = search_result["title"]
+    newsletter["issues"] = []
+
+    for issue in issues:
+        issue_details = {}
+        issue_details["title"] = issue["title"]
+        issue_details["link"] = issue["canonical_url"]
+        issue_details["date"] = issue["post_date"]
+        issue_details["author"] = issue["publishedBylines"][0]["name"]
+        issue_details["content"], issue_details["num_of_images"] = scrape_issue_content(issue["canonical_url"])
+
+        newsletter["issues"].append(issue_details)
+
+    return newsletter
+
+def scrape_issue_content(issue_url: str) -> Tuple[str, int]:
+    r = requests.get(issue_url, timeout=10)
+    if r.status_code != 200:
+        raise HTTPException(status_code=404, detail="Issue not found")
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    article = soup.find("article")
+    if not article:
+        raise HTTPException(status_code=404, detail="Content not found")
+
+    for widget in article.select("div.subscription-widget-wrap"):
+        widget.decompose()
+
+    issue_content = article.find("div", class_="available-content")
+    paras = [p.text.strip() for p in issue_content.find_all("p")]
+    num_of_images = len(issue_content.find_all("img"))
+
+    sanitized_paras = sanitize_content(paras)
+
+    return sanitized_paras, num_of_images
+
+def sanitize_content(content: List[str]) -> str:
+    sanitized_paras = [ paragraph for paragraph in content if paragraph ]
+    content_str = "\n\n".join(sanitized_paras)
+    return content_str
