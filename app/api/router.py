0ac7ef395d4a5c67e90437d90a0ef65582dbe112
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2025-11-29 02:27:08 +0530

    fix: removed placeholder URL

--- app/api/router.py
+++ app/api/router.py
@@ -1,11 +1,10 @@
 import logging
 from fastapi import APIRouter
 from app.db import queries
-from app.llm.models import AnalysisResponse
 from app.tasks.pipeline import scraping_stage
 from app.tasks.newsletter_tasks import aggregate_issue_analysis
 
-
+logger = logging.getLogger(__name__)
 router = APIRouter(tags=["newsletter"])
 
 search_results = [
@@ -52,7 +51,6 @@ search_results = [
     }
 ]
 
-logger = logging.getLogger(__name__)
 
 @router.get("")
 async def health_check():
@@ -77,55 +75,6 @@ def trigger_analysis():
     scraping_stage.delay(analysis_run_id, search_results)
 
     return { "status": "ok", "message": "Analysis triggered" }
-
-@router.get("/test-analysis-insert")
-def test_analysis_insert():
-    analysis = AnalysisResponse(
-        title="Football and tail risks",
-        subtitle="Let's not forget the lessons here",
-        author="",
-        word_count=1035,
-        image_count=1,
-        like_count=5,
-        comment_count=12,
-        section_count=10,
-        emoji_count=0,
-        title_emoji_count=0,
-        subtitle_emoji_count=0,
-        title_word_count=4,
-        subtitle_word_count=6,
-        addressed_user_by_name=False,
-        reading_time_minutes=4,
-        product_mention_count=0,
-        ctas=[
-            LinkObject(
-                type="reference",
-                text="normal distribution",
-                url="https://www.investopedia.com/terms/n/normaldistribution.asp"
-            ),
-            LinkObject(
-                type="reference",
-                text="Investopedia",
-                url="https://www.investopedia.com/terms/t/tailrisk.asp"
-            ),
-            LinkObject(
-                type="reference",
-                text="BBC story on the underground caves inhabited during WW2",
-                url="https://www.bbc.com/news/magazine-34139311"
-            ),
-        ],
-        ads=[],
-        overall_summary=(
-            "The newsletter discusses the concept of tail risks in football, emphasizing "
-                "the need for better preparedness in the face of unexpected events, drawing "
-                "parallels with historical crises and urging clubs to collaborate for solutions."
-        ),
-        overall_intent="to inform",
-        overall_tone="thoughtful"
-    )
-
-    queries.insert_issue_analysis(1, analysis)
-    return { "status": "ok", "message": "Test analysis inserted" }
 
 @router.get("/get-analysis")
 def get_analysis():
